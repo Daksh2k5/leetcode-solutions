0f966d1430f4c354acc7d098bc7f9b422e994114
Daksh Vyas <116201275+Daksh2k5@users.noreply.github.com>
2026-07-06 00:12:33 +0530

    Delete 242-ValidAnagram/version/python/242-ValidAnagram_20260705_235207.py

--- 242-ValidAnagram/version/python/242-ValidAnagram_20260705_235207.py
+++
@@ -1,8 +0,0 @@
-# Last updated: 7/5/2026, 11:52:07 PM
-1class Solution:
-2    def isAnagram(self, s: str, t: str) -> bool:
-3        s=list(s)
-4        t=list(t)
-5        s.sort()
-6        t.sort()
-7        return s==t 
