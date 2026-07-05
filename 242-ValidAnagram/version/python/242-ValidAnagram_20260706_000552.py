1b7f7c3c7bd94757c1fe342c07bae0b92800c5ae
Daksh Vyas <116201275+Daksh2k5@users.noreply.github.com>
2026-07-06 00:13:50 +0530

    Refactor isAnagram for improved readability

--- 242-ValidAnagram/version/python/242-ValidAnagram_20260706_000552.py
+++ 242-ValidAnagram/version/python/242-ValidAnagram_20260706_000552.py
@@ -1,5 +1,4 @@
-# Last updated: 7/6/2026, 12:05:52 AM
-# shorter code
-1class Solution:
-2    def isAnagram(self, s: str, t: str) -> bool:
-3        return sorted(list(s))==sorted(list(t))
+# Last updated: 7/6/2026, 12:13:30 AM
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s)==sorted(t)
