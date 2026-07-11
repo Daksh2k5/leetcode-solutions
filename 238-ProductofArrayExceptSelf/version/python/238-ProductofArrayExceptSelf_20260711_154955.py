7306c834d8e56c1c902248ddef113942ddcf4318
Daksh Vyas <116201275+Daksh2k5@users.noreply.github.com>
2026-07-11 15:58:15 +0530

    Delete 238-ProductofArrayExceptSelf/version/python directory

--- 238-ProductofArrayExceptSelf/version/python/238-ProductofArrayExceptSelf_20260711_154955.py
+++
@@ -1,7 +0,0 @@
-# Last updated: 7/11/2026, 3:49:55 PM
-1class Solution:
-2    def strStr(self, haystack: str, needle: str) -> int:
-3        if needle in haystack:
-4            return haystack.index(needle)
-5        else:
-6            return -1
