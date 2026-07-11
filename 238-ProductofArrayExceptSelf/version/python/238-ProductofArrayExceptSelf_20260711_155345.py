2ce2b93a71fc84b783e4a7a16525c96fb23709af
Daksh Vyas <116201275+Daksh2k5@users.noreply.github.com>
2026-07-11 15:53:46 +0530

    Create 238-ProductofArrayExceptSelf

---
+++ 238-ProductofArrayExceptSelf/version/python/238-ProductofArrayExceptSelf_20260711_155345.py
@@ -0,0 +1,7 @@
+# Last updated: 7/11/2026, 3:53:45 PM
+1class Solution:
+2    def strStr(self, haystack: str, needle: str) -> int:
+3        try:
+4            return haystack.index(needle)
+5        except ValueError:
+6            return -1
