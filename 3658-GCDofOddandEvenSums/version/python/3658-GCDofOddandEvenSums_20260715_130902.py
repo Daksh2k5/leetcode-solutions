0adf90cfa6fd4d5173093d6aaf97fbdf91722b34
Daksh Vyas <116201275+Daksh2k5@users.noreply.github.com>
2026-07-15 13:09:03 +0530

    Create 3658-GCDofOddandEvenSums

---
+++ 3658-GCDofOddandEvenSums/version/python/3658-GCDofOddandEvenSums_20260715_130902.py
@@ -0,0 +1,5 @@
+# Last updated: 7/15/2026, 1:09:02 PM
+# i cannot believe i spent 5 minutes on this before realising the answer
+1class Solution:
+2    def gcdOfOddEvenSums(self, n: int) -> int:
+3        return n
