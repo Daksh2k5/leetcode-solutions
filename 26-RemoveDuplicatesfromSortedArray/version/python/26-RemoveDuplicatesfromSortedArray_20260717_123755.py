f2d208918ca99c5d5b48e7fca76bd4160dfb9bec
Daksh Vyas <116201275+Daksh2k5@users.noreply.github.com>
2026-07-17 12:37:55 +0530

    Create 26-RemoveDuplicatesfromSortedArray

---
+++ 26-RemoveDuplicatesfromSortedArray/version/python/26-RemoveDuplicatesfromSortedArray_20260717_123755.py
@@ -0,0 +1,9 @@
+# Last updated: 7/17/2026, 12:37:55 PM
+1class Solution:
+2    def removeDuplicates(self, nums: List[int]) -> int:
+3        s=sorted(set(nums))
+4        for i in range(len(nums)):
+5            nums.pop()
+6        for i in s:
+7            nums.append(i)
+8        return (len(nums))
