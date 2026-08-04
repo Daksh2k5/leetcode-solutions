8d59f9c861d38b9370d29d8caab7848e8047e777
Daksh Vyas <116201275+Daksh2k5@users.noreply.github.com>
2026-08-04 08:28:20 +0530

    Create 1021-RemoveOutermostParentheses

---
+++ 1021-RemoveOutermostParentheses/version/python/1021-RemoveOutermostParentheses_20260804_082819.py
@@ -0,0 +1,15 @@
+# Last updated: 8/4/2026, 8:28:19 AM
+1class Solution:
+2    def removeOuterParentheses(self, s: str) -> str:
+3        ans = []
+4        count = 0
+5        for i in s:
+6            if i == '(':
+7                if count > 0:
+8                    ans.append(i)
+9                count +=1
+10            else:
+11                count -= 1
+12                if count > 0:
+13                    ans.append(i)
+14        return ''.join(ans)
