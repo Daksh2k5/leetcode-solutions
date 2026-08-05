6df404d27723cbb0389cc4601f74d99d9b372f1b
Daksh Vyas <116201275+Daksh2k5@users.noreply.github.com>
2026-08-05 12:05:19 +0530

    Delete problem-1785911671993/version/python directory

--- problem-1785911671993/version/python/problem-1785911671993_20260805_120431.py
+++
@@ -1,8 +0,0 @@
-# Last updated: 8/5/2026, 12:04:31 PM
-1from collections import Counter
-2class Solution:
-3    def containsDuplicate(self, nums: List[int]) -> bool:
-4        c=Counter(nums).most_common()
-5        if c[0][1]>1:
-6            return True
-7        return False
