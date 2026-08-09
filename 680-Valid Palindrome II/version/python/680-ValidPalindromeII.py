171c1a70528550fa7c3ea07c3e30ed6477d0fbfa
Daksh Vyas <daksh2k5@gmail.com>
2026-08-09 12:32:27 +0530

    Add README for 680-ValidPalindromeII

---
+++ 680-Valid Palindrome II/version/python/680-ValidPalindromeII.py
@@ -0,0 +1,13 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        left=0
+        right=len(s)-1
+        while left<right:
+            if s[left]!=s[right]:
+                if s[left:right]==s[left:right][::-1] or s[left+1:right+1]==s[left+1:right+1][::-1]:
+                    return True
+                else:
+                    return False
+            left+=1
+            right-=1
+        return True
