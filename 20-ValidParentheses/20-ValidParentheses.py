fdb4f877bce9d6fa74175eddcf70e06599646cef
Daksh Vyas <116201275+Daksh2k5@users.noreply.github.com>
2026-08-09 12:38:00 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -0,0 +1,31 @@
+# Last updated: 8/9/2026, 12:37:59 PM
+class Solution:
+    def isValid(self, s: str) -> bool:
+        l=['z']
+        for i in s:
+            if i == '(' or i == '{' or i == '[':
+                l.append(i)
+            if i == ')':
+                if '(' not in l:
+                    return False
+            if i == '}':
+                if '{' not in l:
+                    return False
+            if i == ']':
+                if '[' not in l:
+                    return False
+
+
+            if i == ')':
+                if l[-1]=='(':
+                    l.pop()
+            if i == '}': 
+                if l[-1]=='{':
+                    l.pop()
+            if i == ']': 
+                if l[-1]=='[':
+                    l.pop()
+        print(l)
+        if len(l)==1:
+            return True
+        return False
