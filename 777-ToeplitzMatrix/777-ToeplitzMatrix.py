52f640ac88e1d63b4a4fa6f6b7aab44f31cfefb5
Daksh Vyas <116201275+Daksh2k5@users.noreply.github.com>
2026-08-09 12:36:16 +0530

    Create 777-ToeplitzMatrix

---
+++ 777-ToeplitzMatrix/777-ToeplitzMatrix.py
@@ -0,0 +1,9 @@
+# Last updated: 8/9/2026, 12:36:15 PM
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        for i in range(len(matrix)-1):
+            for j in range(len(matrix[0])-1):
+                if matrix[i][j]!=matrix[i+1][j+1]:
+                    return False
+        return True     
+        
