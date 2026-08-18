ad1d926abeb722a885f797fab0545b275119970e
Daksh Vyas <116201275+Daksh2k5@users.noreply.github.com>
2026-08-18 13:24:56 +0530

    File updated at 8/18/2026, 1:24:56 PM

--- 125-ValidPalindrome/125-ValidPalindrome.py
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -1,5 +1,19 @@
-# Last updated: 8/9/2026, 12:37:15 PM
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        ans="".join([x for x in s if x.isalnum()]).lower()
-        return (ans==ans[::-1])
+# Last updated: 8/18/2026, 1:24:56 PM
+1class Solution:
+2    def isPalindrome(self, s: str) -> bool:
+3        s=s.lower()
+4        left=0
+5        right=len(s)-1
+6        while left<right:
+7            if not s[left].isalnum():
+8                left+=1
+9                continue
+10            if not s[right].isalnum():
+11                right-=1
+12                continue
+13            if s[left]!=s[right]:
+14                return False
+15            else:
+16                left+=1
+17                right-=1
+18        return True
