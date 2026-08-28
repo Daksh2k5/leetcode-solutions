c6353757d7aba3f2d3024f57794342ca7db5e6fa
Daksh Vyas <116201275+Daksh2k5@users.noreply.github.com>
2026-08-28 23:43:44 +0530

    File updated at 8/28/2026, 11:43:45 PM

--- 205-IsomorphicStrings/205-IsomorphicStrings.py
+++ 205-IsomorphicStrings/205-IsomorphicStrings.py
@@ -1,4 +1,4 @@
-# Last updated: 8/23/2026, 4:34:52 PM
+# Last updated: 8/28/2026, 11:43:45 PM
 1class Solution:
 2    def isIsomorphic(self, s: str, t: str) -> bool:
-3        return [s.find(c) for c in s] == [t.find(c) for c in t]
+3        return [s.find(x) for x in s] == [t.find(x) for x in t]
