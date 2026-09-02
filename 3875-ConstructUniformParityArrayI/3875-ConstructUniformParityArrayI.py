0952b8c98f4ebb33b770ed2af2b8918a09dfd097
Daksh Vyas <116201275+Daksh2k5@users.noreply.github.com>
2026-09-02 14:23:37 +0530

    File updated at 9/2/2026, 2:23:38 PM

--- 3875-ConstructUniformParityArrayI/3875-ConstructUniformParityArrayI.py
+++ 3875-ConstructUniformParityArrayI/3875-ConstructUniformParityArrayI.py
@@ -1,4 +1,4 @@
-# Last updated: 9/2/2026, 2:23:03 PM
-1class Solution:
-2    def uniformArray(self, nums1: list[int]) -> bool:
-3        return True
+# Last updated: 9/2/2026, 2:23:38 PM
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
