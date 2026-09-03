8f0c5209d15b2daa7d38390b46db78900adc09c0
Daksh Vyas <116201275+Daksh2k5@users.noreply.github.com>
2026-09-03 09:31:38 +0530

    Create 3110-ScoreofaString

---
+++ 3110-ScoreofaString/3110-ScoreofaString.py
@@ -0,0 +1,7 @@
+# Last updated: 9/3/2026, 9:31:38 AM
+1class Solution:
+2    def scoreOfString(self, s: str) -> int:
+3        score=0
+4        for i in range(len(s)-1):
+5            score+= abs(ord(s[i])-ord(s[i+1]))
+6        return score
