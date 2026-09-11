b1562b56b23055d6c849984950b006594e212de0
Daksh Vyas <116201275+Daksh2k5@users.noreply.github.com>
2026-09-11 15:09:23 +0530

    Create 3483-Unique3-DigitEvenNumbers

---
+++ 3483-Unique3-DigitEvenNumbers/3483-Unique3-DigitEvenNumbers.py
@@ -0,0 +1,11 @@
+# Last updated: 9/11/2026, 3:09:23 PM
+1class Solution:
+2    def totalNumbers(self, digits: List[int]) -> int:
+3        l=[]
+4        p=permutations(digits,r=3)
+5        for i in p:
+6            if i[0]!=0:
+7                if i[-1]%2==0:
+8                    if i not in l:
+9                        l.append(i)
+10        return len(l)
