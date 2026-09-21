0cb4c094422697638ee76306cb2533253cee7501
Daksh Vyas <116201275+Daksh2k5@users.noreply.github.com>
2026-09-21 15:55:42 +0530

    Create 746-MinCostClimbingStairs

---
+++ 746-MinCostClimbingStairs/746-MinCostClimbingStairs.py
@@ -0,0 +1,10 @@
+# Last updated: 9/21/2026, 3:55:43 PM
+1class Solution:
+2    def minCostClimbingStairs(self, cost: list[int]) -> int:
+3        dp=cost.copy()
+4        dp.append(0)
+5        for i in range(len(cost)-2,-1,-1):
+6            # print(dp[i])
+7            dp[i]+=min(dp[i+1],dp[i+2])
+8        # print(dp)
+9        return min(dp[0],dp[1])
