f791a9e424f00373888f7d7b7a65ff6557e51180
Daksh Vyas <116201275+Daksh2k5@users.noreply.github.com>
2026-09-24 18:44:31 +0530

    File updated at 9/24/2026, 6:44:32 PM

--- 3550-SmallestIndexWithDigitSumEqualtoIndex/3550-SmallestIndexWithDigitSumEqualtoIndex.py
+++ 3550-SmallestIndexWithDigitSumEqualtoIndex/3550-SmallestIndexWithDigitSumEqualtoIndex.py
@@ -1,4 +1,4 @@
-# Last updated: 9/24/2026, 6:44:22 PM
+# Last updated: 9/24/2026, 6:44:32 PM
 1class Solution:
 2    def smallestIndex(self, nums: List[int]) -> int:
 3        for i in range(len(nums)):
