a6dd89b33b2574b051795b3a8c82cf3ae33bbcf9
Daksh Vyas <116201275+Daksh2k5@users.noreply.github.com>
2026-09-26 22:20:08 +0530

    File updated at 9/26/2026, 10:20:07 PM

--- 904-FruitIntoBaskets/904-FruitIntoBaskets.py
+++ 904-FruitIntoBaskets/904-FruitIntoBaskets.py
@@ -1,27 +1,27 @@
-# Last updated: 8/9/2026, 12:36:09 PM
-class Solution:
-    def totalFruit(self, fruits: List[int]) -> int:
-        count = {}
-        left = 0
-        ans = 0
-
-        for right in range(len(fruits)):
-            fruit = fruits[right]
-
-            if fruit in count:
-                count[fruit] += 1
-            else:
-                count[fruit] = 1
-
-            while len(count) > 2:
-                leftFruit = fruits[left]
-                count[leftFruit] -= 1
-
-                if count[leftFruit] == 0:
-                    del count[leftFruit]
-
-                left += 1
-
-            ans = max(ans, right - left + 1)
-
-        return ans
+# Last updated: 9/26/2026, 10:20:07 PM
+1class Solution:
+2    def totalFruit(self, fruits: List[int]) -> int:
+3        count = {}
+4        left = 0
+5        ans = 0
+6
+7        for right in range(len(fruits)):
+8            fruit = fruits[right]
+9
+10            if fruit in count:
+11                count[fruit] += 1
+12            else:
+13                count[fruit] = 1
+14
+15            while len(count) > 2:
+16                leftFruit = fruits[left]
+17                count[leftFruit] -= 1
+18
+19                if count[leftFruit] == 0:
+20                    del count[leftFruit]
+21
+22                left += 1
+23
+24            ans = max(ans, right - left + 1)
+25
+26        return ans
